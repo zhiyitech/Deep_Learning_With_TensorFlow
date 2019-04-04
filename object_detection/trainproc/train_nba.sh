d0abd58769110b0c32323829084b2cd875c55fdd
gusuperstar <gusuperstar@gmail.com>
2019-04-04 17:49:25 +0800

    Update train_nba.sh

--- object_detection/trainproc/train_nba.sh
+++ object_detection/trainproc/train_nba.sh
@@ -1,17 +1,3 @@
-#wget http://www.robots.ox.ac.uk/~vgg/data/pets/data/images.tar.gz
-#wget http://www.robots.ox.ac.uk/~vgg/data/pets/data/annotations.tar.gz
-#tar -xvf images.tar.gz
-#tar -xvf annotations.tar.gz
-
-#python object_detection/dataset_tools/create_pet_tf_record.py \
-#    --label_map_path=object_detection/data/pet_label_map.pbtxt \
-#    --data_dir=`pwd` \
-#    --output_dir=`pwd`
-
-#wget http://storage.googleapis.com/download.tensorflow.org/models/object_detection/faster_rcnn_resnet101_coco_11_06_2017.tar.gz
-#tar -xvf faster_rcnn_resnet101_coco_11_06_2017.tar.gz
-#mv faster_rcnn_resnet101_coco_11_06_2017/model.ckpt.* data/
-
 # From the tensorflow/models/research/ directory
 PIPELINE_CONFIG_PATH="/data/home/goosegu/video/models/research/data/nba/faster_rcnn_resnet101_nba.config"  #{path to pipeline config file}
 MODEL_DIR=models_nba
