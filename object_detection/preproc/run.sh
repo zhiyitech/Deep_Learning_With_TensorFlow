3efe10159e03f242677a7114bd8c3a30a42135ec
gusuperstar <gusuperstar@gmail.com>
2019-04-04 16:56:14 +0800

    Update run.sh

--- object_detection/preproc/run.sh
+++ object_detection/preproc/run.sh
@@ -1,2 +1,4 @@
+#将labelimg输出的xml格式标注数据，转换为更容易读取的csv格式
 python xml_to_csv.py
+#结合csv标注的视频leonard数据和videoclip第一步生成视频帧图片目录的数据，生成模型输入所需的tfrecord文件
 python generate_tfrecord.py --csv_input=leonard_label.csv  --output_path=leonard_labels.record
