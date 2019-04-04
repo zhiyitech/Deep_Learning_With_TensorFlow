c6dbf70a12c364760900996e760346fc3aa8cda0
/bin/eash <goosegu@TENCENT64.site>
2019-04-04 16:11:57 +0800

    object_detection add clip

---
+++ object_detection/videoclip/cut.py
@@ -0,0 +1,24 @@
+import sys
+import argparse
+import cv2
+print(cv2.__version__)
+
+def extractImages(pathIn, pathOut):
+    count = 0
+    vidcap = cv2.VideoCapture(pathIn)
+    success,image = vidcap.read()
+    success = True
+    while success:
+        print ('Read a new frame: ', success)
+        if count % 5 == 0:
+            cv2.imwrite( pathOut + "/frame%05d.jpg" % count, image)     # save frame as JPEG file
+        count = count + 1
+        success,image = vidcap.read()
+
+if __name__=="__main__":
+    a = argparse.ArgumentParser()
+    a.add_argument("--input", help="path to video")
+    a.add_argument("--output", help="path to images")
+    args = a.parse_args()
+    print(args)
+    extractImages(args.input, args.output)
---
+++ object_detection/videoclip/run.sh
@@ -0,0 +1,2 @@
+mkdir res
+python cut.py --input ../../data/wolves/16.mp4 --output res 
